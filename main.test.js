dc6026130f969bbf969731aa5b4c48776ac5b4e6
Matheus <matheuspl92@hotmail.com>
2022-11-23 11:58:03 -0300

    Add reverseString function and its tests

--- main.test.js
+++ main.test.js
@@ -1,5 +1,6 @@
 const main = require('./main');
 const capitalize = require('./capitalize');
+const reverseString = require('./reverseString');
 
 
 test('adds 1 + 2 to equal 3', () => {
@@ -16,4 +17,16 @@ test('capitalize first letter in bRaZiL returning BRaZiL', () => {
 
 test('capitalize first letter in ELEPHANT returning ELEPHANT', () => {
     expect(capitalize('ELEPHANT')).toBe('ELEPHANT');
+});
+
+test('reverse string "My name is John Cena" to "aneC nhoJ si eman yM"', () => {
+    expect(reverseString('My name is John Cena')).toBe('aneC nhoJ si eman yM');
+});
+
+test('reverse string "arara" to "arara"', () => {
+    expect(reverseString('arara')).toBe('arara');
+});
+
+test('reverse string "SENTA A PUA" to "AUP A ATNES"', () => {
+    expect(reverseString('SENTA A PUA')).toBe('AUP A ATNES');
 });
